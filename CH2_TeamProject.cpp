fee9660c82c1904001036c4c2e25aeecea5d471d
kkym323 <kkym32387@gmail.com>
2026-01-19 14:28:50 +0900

    캐릭터구현

---
+++ CH2_TeamProject.cpp
@@ -0,0 +1,15 @@
+#include "Character/Character.h"
+
+int main()
+{
+
+    ACharacter* MyChar = new ACharacter();
+
+    MyChar->Attack();
+
+    MyChar->TakeDamage(30);
+
+    delete MyChar;
+
+    return 0;
+}
