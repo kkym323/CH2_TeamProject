29eb0d959a55f19fdc7497ab673ec6f5259da6eb
kkym323 <kkym32387@gmail.com>
2026-01-22 12:21:27 +0900

    Commit : 크리티컬 데미지 구현

--- Character/Character.cpp
+++ Character/Character.cpp
@@ -1,10 +1,27 @@
 #include "Character.h"
+#include <random>
 
-ACharacter::ACharacter(string NewName, int NewHp, int NewAtk)
+int getRandomInt()
+{
+    // 시드값 생성기 (한 번만 초기화하기 위해 static 사용)
+    static std::random_device rd;
+
+    // 메르센 트위스터 엔진 (고품질 난수 생성기)
+    static std::mt19937 gen(rd());
+
+    // 0부터 100까지 균등하게 분포 (양 끝값 포함)
+    std::uniform_int_distribution<int> dis(0, 100);
+
+    return dis(gen);
+}
+
+ACharacter::ACharacter(string NewName, int NewHp, int NewAtk, int NewDef, int NewCritical)
 {
     Name = NewName;
     Hp = NewHp;
     Atk = NewAtk;
+    Def = NewDef;
+    Critical = NewCritical;
 
     cout << "[생성] " << Name << "가 전장에 나타났습니다! (HP: " << Hp << ")" << endl;
 }
@@ -16,20 +33,40 @@ ACharacter::~ACharacter()
 
 void ACharacter::Attack(ACharacter* Target)
 {
-    cout << Name << "가 공격합니다! (공격력: " << Atk << ")";
-    Target->TakeDamage(Atk);
+    if (getRandomInt() <= 10)
+    {
+        int Crtical_Atk = Atk * 15 / 10;
+        cout << Name << "가 공격합니다![크리티컬!] (공격력: " << Crtical_Atk << ")" << endl;
+        Target->TakeDamage(Crtical_Atk);
+    }
+    else
+    {
+        cout << Name << "가 공격합니다! (공격력: " << Atk << ")" << endl;
+        Target->TakeDamage(Atk);
+    }
+
 }
 
 void ACharacter::TakeDamage(int DamageAmount)
 {
-    Hp = Hp - DamageAmount;
-    if (Hp < 0)
+    if (Def >= DamageAmount)
     {
-        Hp = 0;
+        DamageAmount = 0;
+        cout << Name << "가 " << DamageAmount << "의 피해를 입었습니다." << endl;
+        cout << "   -> 남은 체력: " << Hp << endl;
     }
-
-    cout << Name << "가 " << DamageAmount << "의 피해를 입었습니다." << endl;
-    cout << "   -> 남은 체력: " << Hp << endl;
+    else
+    {
+        int Damage = DamageAmount - Def;
+        Hp = Hp - Damage;
+        if (Hp < 0)
+        {
+            Hp = 0;
+        }
+        cout << Name << "가 " << Damage << "의 피해를 입었습니다." << endl;
+        cout << "   -> 남은 체력: " << Hp << endl;
+        
+    }
 }
 
 int ACharacter::GetHp()
@@ -47,3 +84,4 @@ bool ACharacter::IsDead()
     else
         return false;
 }
+
--- Character/Character.h
+++ Character/Character.h
@@ -1,22 +1,21 @@
-// 파일명: Character.h
 #pragma once
 #include <iostream>
-#include <string> // 문자열(이름)을 쓰기 위해 필요합니다.
+#include <string>
 
 using namespace std;
 
-// [Mission 1] 클래스 선언
-// 언리얼 표준: 클래스 이름은 'A'로 시작합니다.
 class ACharacter
 {
 public:
-    ACharacter(string NewName, int NewHp, int NewAtk);
+    ACharacter(string NewName, int NewHp, int NewAtk, int NewDef, int NewCritical);
     ~ACharacter();
 
 protected:
     string Name;
     int Hp;
     int Atk;
+    int Def;
+    int Critical;
 
 public:
     void Attack(ACharacter* Target);
