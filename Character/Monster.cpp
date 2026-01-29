a851e02f46d4171651108b10eae1d1e392a2bf4a
kkym323 <kkym32387@gmail.com>
2026-01-29 10:24:27 +0900

    [실습] 01. 가상 함수: 상속을 이용하자

--- Character/Monster.cpp
+++ Character/Monster.cpp
@@ -6,3 +6,10 @@ AMonster::AMonster(const string& NewName, const FUnitStat& NewStat)
 {
 	
 }
+
+void AMonster::Attack(ACharacter* Target)
+{
+	cout << "크아앙! " << Name << "이(가) 이빨로 물어뜯습니다!" << endl;
+
+	Target->TakeDamage(Stat.Atk);
+}
