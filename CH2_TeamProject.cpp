ff0671f55d1173f056c68c56e486659fa4781788
kkym323 <kkym32387@gmail.com>
2026-02-12 11:07:06 +0900

    [실습] 01. main 함수 대청소

--- CH2_TeamProject.cpp
+++ CH2_TeamProject.cpp
@@ -4,43 +4,48 @@
 #include "Character/Monster.h"
 using namespace std;
 
+ACharacter* Player = new APlayer("나의 용사", { 200, 50, 30, 5, 10 });
+ACharacter* Monster = new AMonster("무서운 오크", { 100, 30, 20, 3, 10 });
+
+
 void WaitForPlayerInput()
 {
     system("pause");
     cout << endl;
 }
 
+bool BattleTurn(ACharacter* Attacker, ACharacter* Defender) 
+{
+        Attacker->PlayTurn(Defender);
+        WaitForPlayerInput();
+
+        if (Defender->IsDead())
+        {
+            cout << Defender->GetName() << "가 쓰러졌습니다! "<< Attacker->GetName() << "의 승리!" << endl;
+            return true;
+        }
+        return false;
+}
+
 int main()
 {
-
-    ACharacter* Player = new APlayer("나의 용사", {200, 50, 30, 5, 10});
-    ACharacter* Monster = new AMonster("무서운 오크", { 100, 30, 20, 3, 10 });
-
     cout << "===  데스매치 시작!  ===" << endl;
     WaitForPlayerInput();
 
-    while (!Player->IsDead() && !Monster->IsDead())
+    while (true)
     {
-        Player->PlayTurn(Monster);
-
-        if (Monster->IsDead())
+        if (BattleTurn(Player, Monster) == true)
         {
-            cout << "몬스터가 쓰러졌습니다! 승리!" << endl;
             break;
         }
 
-        WaitForPlayerInput();
-
-        Monster->PlayTurn(Player);
-
-        if (Player->IsDead())
+        if (BattleTurn(Monster, Player) == true)
         {
-            cout << "플레이어가 쓰러졌습니다... 패배..." << endl;
             break;
         }
-
-        WaitForPlayerInput();
     }
+
+    WaitForPlayerInput();
 
     delete Player;
     delete Monster;
